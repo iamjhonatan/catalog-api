0b0110331161ba1b7a477f614ae0d1ba8617e6f0
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2023-01-03 20:41:52 -0300

    class 30: registering the services in the Program class

--- CatalogAPI/Program.cs
+++ CatalogAPI/Program.cs
@@ -1,8 +1,17 @@
+using CatalogAPI.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection,
+        ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
 
