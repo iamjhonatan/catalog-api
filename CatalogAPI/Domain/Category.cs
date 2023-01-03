449ff785bbbb0c5f6ffd655acd22b17f4d505d27
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2023-01-03 20:17:03 -0300

    class 27: creating domain entities

---
+++ CatalogAPI/Domain/Category.cs
@@ -0,0 +1,8 @@
+namespace CatalogAPI.Domain;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? UrlImage { get; set; }
+}
---
+++ CatalogAPI/Domain/Product.cs
@@ -0,0 +1,12 @@
+namespace CatalogAPI.Domain;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? UrlImage { get; set; }
+    public float Stock { get; set; }
+    public DateTime RegistrationDate { get; set; }
+}
