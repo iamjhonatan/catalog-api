ccbaeeb7c8515a6f1a709c0fb6c342e534cfc598
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2023-01-03 20:09:47 -0300

    class 26: project created

--- CatalogAPI/Program.cs
+++ CatalogAPI/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+    app.UseSwagger();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
