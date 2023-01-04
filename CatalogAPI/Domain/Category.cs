c3b6ca46f7d34accb4c25c6cd9a0a2e8efc2b427
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2023-01-04 20:53:41 -0300

    class 34: presenting DataAnnotations resource

--- CatalogAPI/Domain/Category.cs
+++ CatalogAPI/Domain/Category.cs
@@ -1,7 +1,10 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CatalogAPI.Domain;
 
+[Table("Categories")]
 public class Category
 {
     public Category()
@@ -9,8 +12,15 @@ public class Category
         Products = new Collection<Product>();
     }
     
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(80)]
     public string? Name { get; set; }
+    
+    [Required]
+    [MaxLength(300)]
     public string? UrlImage { get; set; }
 
     
--- CatalogAPI/Domain/Product.cs
+++ CatalogAPI/Domain/Product.cs
@@ -1,12 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CatalogAPI.Domain;
 
+[Table("Products")]
 public class Product
 {
+    [Key]
     public int ProductId { get; set; }
+    
+    [Required]
+    [MaxLength(80)]
     public string? Name { get; set; }
+    
+    [Required]
+    [StringLength(300)]
     public string? Description { get; set; }
+    
+    [Required]
+    [Column(TypeName = "decimal(10,2")]
     public decimal Price { get; set; }
+    
+    [Required]
+    [StringLength(300)]
     public string? UrlImage { get; set; }
+    
     public float Stock { get; set; }
     public DateTime RegistrationDate { get; set; }
 
