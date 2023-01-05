8d37bc6ae8562c92ba9c07b1bdf75d8d21e949ac
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2023-01-04 21:25:09 -0300

    class 37: adding categories data

---
+++ CatalogAPI/Migrations/20230105002041_AddCategoriesData.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogAPI.Migrations
+{
+    public partial class AddCategoriesData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(Name, UrlImage) VALUES ('Bebidas', 'bebidas.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, UrlImage) VALUES ('Lanches', 'lanches.jpg')");
+            migrationBuilder.Sql("INSERT INTO Categories(Name, UrlImage) VALUES ('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories");
+        }
+    }
+}
